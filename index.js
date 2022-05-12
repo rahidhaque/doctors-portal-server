b74eb06761a9d61ca58d63b9e11f9c25e6c2ce12
rahidhaque <ontirahid@yahoo.com>
2022-05-12 18:17:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello From Doctor Uncle!')
+})
+
+
+const uri = "mongodb+srv://doctor_admin:<password>@cluster0.jdbto.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+app.listen(port, () => {
+    console.log(`Doctor port listening on port ${port}`)
+})
